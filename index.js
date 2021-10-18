3fd8126c74eaae499410dd47970de468e71b68c1
Ryan <ryan@MacSammer.local>
2021-10-18 19:47:27 -0300

    Validate

--- index.js
+++ index.js
@@ -3,18 +3,44 @@
 const program = require('commander');
 const package = require('./package.json');
 const shell = require ('shelljs');
+const inquirer = require ('inquirer');
 
 program.version (package.version);
 
 program 
     .command('cadastrar')
-    .argument('[preco]', 'preco do produto')
-    .argument('[marca]', 'marca do produto')
     .description('Cadastra um novo produto')
-    .action((produtos, marca, preco ) => {
-        console.log(produtos);
-        console.log('marca:', marca);
-        console.log('preço:', preco);
-    });
+    .action(async ()  => {
+        let asnwers = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'nome',
+                message: 'Informe o nome do produto',
+                validate: value => value ? true : 'Não é permitido um nome vazio'
+            },
+            {
+                type: 'number',
+                name: 'preco',
+                message: 'Informe o preco: ',
+                validate: value => value ? true : 'Insira um valor para o produto'
+            },
+            {
+                type: 'input',
+                name: 'marca',
+                message: 'Informe a marca: ',
+                validate: value => value ? true : 'Insira a marca do produto'
+            }
+    ]);
 
+let produto = {
+    name: answers.nome,
+    preco: answers.preco,
+    marca: answers.marca
+}
+
+console.log('Produto cadastrado com sucesso' );
+console.log('Nome : ' + produto.nome );
+console.log('Preço: ' + produto.preco );
+console.log('Marca: ' + produto.marca );
+    });
 program.parse(process.argv);
